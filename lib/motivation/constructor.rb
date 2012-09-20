79bbaffbd95001044a397985ace7e170424356d1
Michael Harrington <kibiz0r@gmail.com>
2012-09-20 12:28:07 -0400

    Support calling constructor multiple times

--- lib/motivation/constructor.rb
+++ lib/motivation/constructor.rb
@@ -1,11 +1,11 @@
 module Motivation
   module Constructor
     def constructor(*dependencies)
-      @motivated_attr_map = dependencies.extract_options!
+      my_motivated_attr_map.merge! dependencies.extract_options!
 
       until dependencies.empty?
         dependency = dependencies.shift
-        @motivated_attr_map[dependency] = dependency
+        my_motivated_attr_map[dependency] = dependency
       end
 
       include Motivation::Motivate
@@ -13,11 +13,16 @@ module Motivation
 
     def motivated_attr_map
       super_attrs = superclass.try(:motivated_attr_map)._?({})
-      super_attrs.merge(@motivated_attr_map ||= {})
+      super_attrs.merge my_motivated_attr_map
     end
 
     def motivated_attrs
       motivated_attr_map.keys
+    end
+
+    private
+    def my_motivated_attr_map
+      @motivated_attr_map ||= {}
     end
   end
 end
